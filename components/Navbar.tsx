64a04fc6f8b3b96d354376ed4e7d6f313aeff86a
Binay <binaydas1123@gmail.com>
2025-03-02 12:56:22 +0530

    Root layout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,7 +4,7 @@ import Auth from "./Auth";
 export default function () {
     return <div className="border flex justify-between items-center bg-blue-200 text-black px-4 py-2 text-xl">
         <h1 className="text-3xl m-2">BlogFlow</h1>
-        <div className="border flex md:gap-20 gap-8">
+        <div className="flex md:gap-20 gap-8">
             <Link href={'/'}>Home</Link>
             <Link href={'/'}>Contact</Link>
             <Link href={'/'}>About</Link>
