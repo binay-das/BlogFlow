36b983e1926f761165e3797251c87feceb7ecff6
Binay <binaydas1123@gmail.com>
2025-03-02 12:38:24 +0530

    Navbar

---
+++ components/Navbar.tsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+import Auth from "./Auth";
+
+export default function () {
+    return <div className="border flex justify-between items-center bg-blue-200 text-black px-4 py-2 text-xl">
+        <h1 className="text-3xl m-2">BlogFlow</h1>
+        <div className="border flex md:gap-20 gap-8">
+            <Link href={'/'}>Home</Link>
+            <Link href={'/'}>Contact</Link>
+            <Link href={'/'}>About</Link>
+            <Auth />
+        </div>
+    </div>
+}
